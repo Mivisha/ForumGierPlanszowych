194cd171f7526ad27fa4bc6766277764398b735c
Mivisha <mivisha.net@gmail.com>
2025-10-27 11:02:48 +0100

    Facktory and seeder for genres

---
+++ database/seeders/GenreSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Genre;
+use App\Enums\Auth\RoleType;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class GenreSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Genre::factory()->create([
+            'name' => 'Strategiczna',
+        ]);
+        Genre::factory()->create([
+            'name' => 'Ekonomiczna',
+        ]);
+        Genre::factory()->create([
+            'name' => 'Przygodowa',
+        ]);
+        Genre::factory()->create([
+            'name' => 'Fabularna (RPG)',
+        ]);
+        Genre::factory()->create([
+            'name' => 'Kooperacyjna',
+        ]);
+
+    }
+}
